a8b6591ffc0a78d1b9f10bb547ec6ee146406678
sumitraj6167 <sumitraj6167@gmail.com>
2026-09-07 21:35:33 +0530

    Added reverse of number and table for number program

--- second day.py
+++ second day.py
@@ -7,3 +7,9 @@ while(n>0):
     rev=rev*10+dig
     n=n//10
 print("Reverse of the number:" ,rev)
+
+#Number's Table
+
+n=int(input("Enter the number to print the tables for: "))
+for i in range(1,11):
+    print(n,"x",i,"=",n*i)
