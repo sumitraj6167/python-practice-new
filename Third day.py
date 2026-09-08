e6b276c5aa6c455942e0ca857d1fbf890bfed8e3
sumitraj6167 <sumitraj6167@gmail.com>
2026-09-08 21:25:49 +0530

    Added to check number is prime or not and number's factorial program

--- Third day.py
+++ Third day.py
@@ -5,8 +5,19 @@ k=0
 for i in range(2,a//2+1):
     if(a%i==0):
         k=k+1
-if(k<0):
+if(k<=0):
     print("Number is prime")
 else:
     print("Number isn't prime")
+
+    #number's Factorial.
+n=int(input("Enter number: "))
+fact=1
+while(n>0):
+    fact=fact*n
+    n=n-1
+print("Factorial of the number is: ")
+print(fact)
+      
+
 
