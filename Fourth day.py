df3fd1748a244f3f389ef5e055cbfdfc50c26008
sumitraj6167 <sumitraj6167@gmail.com>
2026-09-09 21:40:29 +0530

    Added sum of first n natural numbers and base-power program

--- Fourth day.py
+++ Fourth day.py
@@ -6,3 +6,15 @@ while(n > 0):
     sum1=sum1+n
     n=n-1
 print("The sum of first n natural numbers is" ,sum1)
+
+#This program determines the base's power given a base and a power.
+
+def power(base,exp):
+    if(exp==1):
+        return(base)
+    if(exp!=1):
+        return(base*power(base,exp-1))
+base=int(input("Enter base: "))
+exp=int(input("Enter exponential value: "))
+print("Result:" ,power(base,exp))
+
