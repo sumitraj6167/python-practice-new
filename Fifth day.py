bab348478f99afc16ff5b2217f060401033cf363
sumitraj6167 <sumitraj6167@gmail.com>
2026-09-10 22:25:59 +0530

    Added celsius to Fahreneit and pascal's triangle with n rows program

--- Fifth day.py
+++ Fifth day.py
@@ -4,3 +4,15 @@ celsius = float(input("Enter temperature in Celsius: "))
 fahrenheit = (celsius * 9/5) + 32
 
 print(f"{celsius}°C is equal to {fahrenheit}°F")
+
+#Pascal's triangle with n rows is printed by the program given a number n.
+
+rows = int(input('Enter the number of rows'))
+# outer Loop
+for i in range(rows):
+    #nested Loop
+    for j in range(i):
+        #display number
+        print(i, end=' ')
+    #new line after each row
+    print('')
