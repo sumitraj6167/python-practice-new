24ec4feaa297893314232664061623af312690e4
sumitraj6167 <sumitraj6167@gmail.com>
2026-09-10 22:33:02 +0530

    Added celsius to Fahreneit and pascal's triangle with n rows and pyramid pattern of numbers program

--- Fifth day.py
+++ Fifth day.py
@@ -16,3 +16,12 @@ for i in range(rows):
         print(i, end=' ')
     #new line after each row
     print('')
+
+#pyramid pattern of numbers.
+
+rows = int(input('Enter the number of rows'))
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print(j, end=' ')
+    print('')
+    
