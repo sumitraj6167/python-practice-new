a6513d1a9e1b49d26a9de65918a5455f7053fe2d
sumitraj6167 <sumitraj6167@gmail.com>
2026-09-11 21:21:02 +0530

    Added inveted pyramid pattern of numbers program

--- Sixth day.py
+++ Sixth day.py
@@ -7,3 +7,12 @@ for i in range(rows, 0, -1):
     for j in range(1, i + 1):
         print(b, end=' ')
     print('\r')
+
+#Inverted pyramid pattern with the same digit.
+
+rows = int(input('Enter the number of rows'))
+num = rows
+for i in range(rows, 0, -1):
+    for j in range(0, i):
+        print(num,end=' ')
+    print("\r")
