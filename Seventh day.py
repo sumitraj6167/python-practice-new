21b74b23cbb5831416f1d0256cc0f2802621fae8
sumitraj6167 <sumitraj6167@gmail.com>
2026-09-12 21:51:30 +0530

    Added to print numbers in a right angled triangle pattern ,another method of right angled triangle and mirror of triangle program

--- Seventh day.py
+++ Seventh day.py
@@ -21,3 +21,18 @@ for i in range(0, rows):
         print("*", end=' ')
     #new line after each row
     print("\r")
+
+#Mirrored right triangle.
+
+rows = int(input("Enter the number of rows "))
+k = 2 * rows - 2
+for i in range(0, rows):
+    #process each column
+    for j in range(0, k):
+        #print space in pyramid
+        print(end=" ")
+    k = k -2
+    for j in range(0, i + 1):
+        #display star
+        print("* ", end="")
+    print("")
