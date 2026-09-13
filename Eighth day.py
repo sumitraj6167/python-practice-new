c2bfed1a78d671707f8f79fff479eaa1179df6ea
sumitraj6167 <sumitraj6167@gmail.com>
2026-09-13 22:08:51 +0530

    Added to make equilateral triangle program

---
+++ Eighth day.py
@@ -0,0 +1,11 @@
+print("print equilateral triangle pyramid using asterisk symbol ")
+size = int(input("Enter the number of rows "))
+m = (2 * size) - 2
+for i in range(0, size):
+    for j in range(0, m):
+        print(end=" ")
+    m = m - 1
+    for j in range(0, i + 1):
+        print("* ", end=' ')
+    print(" ")
+    
