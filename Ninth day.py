1b09b3322012a17be544e9d3268f6062f4b8c7e8
sumitraj6167 <sumitraj6167@gmail.com>
2026-09-14 20:44:41 +0530

    Added to find roots of an equation , find area of triangle and pythagorean triplpet up to the upper limit

--- Ninth day.py
+++ Ninth day.py
@@ -23,3 +23,21 @@ c=int(input("Enter third side: "))
 s=(a+b+c)/2
 area=math.sqrt(s*(s-a)*(s-b)*(s-c))
 print("Area of the triangle is: ",round(area,2))
+
+#All pythagorean triplets up to the upper limit are found by the program after it is given an upper limit.
+
+limit=int(input("Enter upper limit:"))
+c=0
+m=2
+while(c<limit):
+    for n in range(1,m+1):
+        a=m*m-n*n
+        b=2*m*n
+        c=m*m+n*n
+        if (c>limit):
+            break
+        if(a==0 or b==0 or c==0):
+            break
+        print(a,b,c)
+    m=m+1
+
