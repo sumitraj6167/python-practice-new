1f6e12d0acf8652d7750aa56d600946fcbf0f38c
sumitraj6167 <sumitraj6167@gmail.com>
2026-09-17 20:18:09 +0530

    Added addition of two Matrices using Numpy

---
+++ Eleventh day.py
@@ -0,0 +1,9 @@
+#Addition of two Matrices using Numpy.
+
+import numpy as np
+A = np.array([[2, 4], [5, -6]])
+B = np.array([[9, -3], [3, 6]])
+print('Matrix A:\n' ,A)
+print('Matrix B:\n' ,B)
+C = A + B
+print('Sum of A and B Matrix is:\n' ,C)
