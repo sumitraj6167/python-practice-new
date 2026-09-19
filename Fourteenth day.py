f45e52bca1a48dbcafe33c89f7223a1a7425de2f
sumitraj6167 <sumitraj6167@gmail.com>
2026-09-19 20:58:05 +0530

    to read a number and print an identity matrix of the desired size and find sum of series: 1 + 1/2 +1/3 + ..... + 1/N

--- Fourteenth day.py
+++ Fourteenth day.py
@@ -1,3 +1,5 @@
+ #python program to read a number and print an identity matrix of the desired size.
+
 n=int(input("Enter a number: "))
 for i in range(0,n):
     for j in range (0,n):
@@ -6,3 +8,13 @@ for i in range(0,n):
         else:
             print("0",sep=" ",end=" ")
     print()
+
+    #python program to find ssun of series: 1 +1/2 +1/3.......+ 1/N.
+
+n=int(input("Enter the number of terms: "))
+sum1=0
+for i in range(1,n+1):
+    sum1=sum1+(1/i)
+print("The sum of series is",round(sum1,2))
+
+     
