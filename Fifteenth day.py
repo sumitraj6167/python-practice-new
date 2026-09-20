269434ff62907daeaf1c0dccf9e02112376ffb91
sumitraj6167 <sumitraj6167@gmail.com>
2026-09-20 22:04:29 +0530

    Added to find sum of series of different patterns program

---
+++ Fifteenth day.py
@@ -0,0 +1,21 @@
+#python program to find the sun of series: 1 + x^2/2 + x^3/3 + ....... + x^n/n.
+
+n=int(input("Enter the number of terms:"))
+x=int(input("Enter the value of x:"))
+sum1=1
+for i in range(2,n+1):
+    sum1=sum1+((x**i)/i)
+print("The sum of series is",round(sum1,2))
+
+#python program to read a number n and print and compute the series: 1+2+3+4+5+......+n
+
+n=int(input("ENter a number: "))
+a=[]
+for i in range(1,n+1):
+    print(i,sep=" ",end=" ")
+    if(i<n):
+        print("+",sep=" ",end=" ")
+    a.append(i)
+print("=",sum(a))
+
+print()
