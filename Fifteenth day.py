482bbd73b5396bc352e5f7fa66b6565001746a8a
sumitraj6167 <sumitraj6167@gmail.com>
2026-09-20 22:18:17 +0530

    Added to find sum of series of different patterns and find LCM program

--- Fifteenth day.py
+++ Fifteenth day.py
@@ -19,3 +19,17 @@ for i in range(1,n+1):
 print("=",sum(a))
 
 print()
+
+#python program to ind the LCM of two numbers.
+
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+if(a>b):
+    min1=a
+else:
+    min1=b
+while(1):
+    if(min1%a==0 and min1%b==0):
+        print("LCM is:",min1)
+        break
+    min1=min1+1
