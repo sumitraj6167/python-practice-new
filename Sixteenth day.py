6ee35c80b283f1555df3b5ede891763f66f93f71
sumitraj6167 <sumitraj6167@gmail.com>
2026-09-21 22:23:21 +0530

    Added to find GCD and find GCD using Recursion program

--- Sixteenth day.py
+++ Sixteenth day.py
@@ -4,3 +4,15 @@ import math
 a=int(input("Enter the first number:"))
 b=int(input("Enter the second number:"))
 print("The GCD of the two number is", math.gcd(a,b))
+
+#GCD program in python using Recusion.
+
+def gcd(a,b):
+    if(b==0):
+        return a
+    else:
+        return gcd(b,a%b)
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+GCD=gcd(a,b)
+print("GCD is: " ,GCD)
