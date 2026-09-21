b9461d0ba714a0545392a31c57e5145fa8cffefc
sumitraj6167 <sumitraj6167@gmail.com>
2026-09-21 22:15:15 +0530

    Added to find GCD of two numbers program

---
+++ Sixteenth day.py
@@ -0,0 +1,6 @@
+#python program to find the GCD of two numbers.
+
+import math
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("The GCD of the two number is", math.gcd(a,b))
