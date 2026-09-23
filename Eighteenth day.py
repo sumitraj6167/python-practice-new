b26b3cce8b73e83f04566107ba6ca7f5d2e208ec
sumitraj6167 <sumitraj6167@gmail.com>
2026-09-23 21:19:13 +0530

    Added to Reverse a given string program

---
+++ Eighteenth day.py
@@ -0,0 +1,5 @@
+#python program to reverse a given string.
+
+a=str(input("Enter a string: "))
+print("Reverse of the string is:" ,end=' ')
+print(a[::-1])
--- Tenth day.py
+++ Tenth day.py
@@ -6,14 +6,3 @@ a=a+b
 b=a-b
 a=a-b
 print("a is:",a,"b is:",b)
-
-#Addition of two Matrices using Numpy.
-
-import numpy as np
-
-A= np.array([[2, 4], [5, -6]])
-B= np.array([[9, -3], [3, 6]])
-print('Matrix A:\n' ,A)
-print('Matrix B:\n' ,B)
-C = A + B
-print('Sum of A and B Matrix is:\n' ,C)
