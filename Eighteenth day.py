a6c2a7661197badb674e1c7a6cc273a3a5d930c6
sumitraj6167 <sumitraj6167@gmail.com>
2026-09-23 21:26:55 +0530

    Added to Revese a given string using Slicing & Recursion

--- Eighteenth day.py
+++ Eighteenth day.py
@@ -1,5 +1,15 @@
-#python program to reverse a given string.
+#python program to reverse a given string using Slicing.
 
 a=str(input("Enter a string: "))
 print("Reverse of the string is:" ,end=' ')
 print(a[::-1])
+
+#ython program to reverse a given string using Recursion.
+
+def reverse(string):
+    if len(string) == 0:
+        return string
+    else:
+        return reverse(string[1:]) + string[0]
+a= str(input("Enter the string to be reversed:"))
+print('reversed string is ',reverse(a))
